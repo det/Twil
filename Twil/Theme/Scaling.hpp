f1f719945ebea13eb7d08159ae6183e6ff7802fc
Chris Clearwater <chris@detrino.org>
2014-05-25 16:17:57 -0700

    Remove duplicate type aliases

--- Twil/Theme/Scaling.hpp
+++ Twil/Theme/Scaling.hpp
@@ -47,10 +47,6 @@ struct QuadraticFilterT
 	}
 };
 
-using QuadraticInterpFilterT = QuadraticFilterT<std::ratio<1, 1>>;
-using QuadraticApproxFilterT = QuadraticFilterT<std::ratio<5, 10>>;
-using QuadraticMixFilterT = QuadraticFilterT<std::ratio<8, 10>>;
-
 template<typename RatioBT, typename RatioCT>
 struct CubicFilterT
 {
@@ -85,12 +81,6 @@ struct CubicFilterT
 		}
 	}
 };
-
-using CatmullRomFilterT = CubicFilterT<std::ratio<0, 1>, std::ratio<1, 2>>;
-using BSplineFilterT = CubicFilterT<std::ratio<1, 1>, std::ratio<0, 1>>;
-using MitchellFilterT = CubicFilterT<std::ratio<1, 3>, std::ratio<1, 3>>;
-using RobidouxFilterT = CubicFilterT<std::ratio<3782, 10000>, std::ratio<3901, 10000>>;
-using RobidouxSharpFilterT = CubicFilterT<std::ratio<2620, 10000>, std::ratio<3690, 10000>>;
 
 template<std::size_t Lobes>
 struct LanczosFilterT
